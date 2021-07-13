ee74182efe55573ac16a4810e8ede9c75b9540f2
matiaspuletti <pulettimatias@gmail.com>
2021-07-13 20:47:03 +0200

    Connect the buttons and calculate a number that shows different text depending on the number reached

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      number: 0
+    };
+  },
+  methods: {
+    addNumber(num) {
+      this.number = this.number + num;
+    }
+  },
+  computed: {
+    result() {
+      if (this.number < 37) {
+        return 'Not there yet!';
+      } else if (this.number === 37) {
+        return this.number;
+      } else {
+        return 'Too much!';
+      }
+    }
+  }
+});
+
+app.mount('#assignment');
